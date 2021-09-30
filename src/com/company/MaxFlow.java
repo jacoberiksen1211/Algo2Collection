da2b0f97277d88f79d6a443e70b0cd20adf3a75b
jacoberiksen1211 <jacob1211@hotmail.com>
2021-09-30 17:16:36 +0200

    initial maxflow not working

---
+++ src/com/company/MaxFlow.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MaxFlow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int v = Integer.parseInt(scanner.nextLine());
+        int e = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < e; i++) {
+
+        }
+    }
+}
