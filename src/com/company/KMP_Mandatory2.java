11b6299b884ddacb4265cc816533135cdf908ee6
Jacob Berg Eriksen <jacob1211@hotmail.com>
2021-11-19 00:48:14 +0100

    KMP_Mandatory2 working 100point codejudge

--- src/com/company/KMP_Mandatory2.java
+++ src/com/company/KMP_Mandatory2.java
@@ -34,33 +34,156 @@ public class KMP_Mandatory2 {
 
         for (int i = 0; i < numGroundPhases; i++) {
             String[] tempGroundTrickString = scanner.nextLine().split(" ");
+            int[] tempGroundTrick;
             if (i < numGroundPhases-1) {
                 String tempAirTime = scanner.nextLine();
+                tempGroundTrick = new int[tempGroundTrickString.length];
+                for (int j = 0; j < tempGroundTrick.length; j++) {
+                    tempGroundTrick[j] = Integer.parseInt(tempGroundTrickString[j]);
+                }
             }
-
-            int[] tempGroundTrick = new int[tempGroundTrickString.length];
-            for (int j = 0; j < tempGroundTrick.length; j++) {
-                tempGroundTrick[j] = Integer.parseInt(tempGroundTrickString[j]);
+            else {
+                tempGroundTrick = new int[tempGroundTrickString.length];
+                int k = 0;
+                for (int j = tempGroundTrick.length - 1; j >= 0; j--) {
+                    tempGroundTrick[j] = Integer.parseInt(tempGroundTrickString[k++]);
+                }
             }
             groundTricks[i] = tempGroundTrick;
         }
-       /* System.out.println("track:");
+        /*System.out.println("track:");
         for (int i = 0; i < trackLength; i++) {
             System.out.print(track[i] + " ");
         }
-        System.out.println("");
-        System.out.println("groundtricks:");
-        for (int i = 0; i < groundTricks.length; i++) {
-            for (int j = 0; j < groundTricks[i].length; j++) {
-                System.out.print(groundTricks[i][j] + " ");
-            }
-            System.out.println(" ");
-        }*/
+        System.out.println("");*/
+        //printArray(groundTricks, "groundtricks:");
 
         //generate faillinks
-        int[][] failLinks = new int[numGroundPhases][];
+        int[][] failLinks = generateFailLinks(groundTricks);
+        //print faillinks
+        //printArray(failLinks, "failLinks:");
+        int[] matchStart_matchLength = kmpAlgorithm1(groundTricks, failLinks, track);
+        if(matchStart_matchLength[0] == -1){
+            System.out.println("Impossible");
+        }
+        else{
+            int stop = matchStart_matchLength[0] + matchStart_matchLength[1];
+            int matchStop = kmpAlgorithm2(groundTricks, failLinks, track, stop);
+            if(matchStop == -1){
+                System.out.println("Impossible");
+            }
+            else{
+                System.out.println(matchStart_matchLength[0] + " " + (matchStop - matchStart_matchLength[0]));
+            }
+        }
+
+    }
+
+    private static int kmpAlgorithm2(int[][] groundTricks, int[][] failLinks, int[] track, int stop) {
+        //KMP algorithm:
+        int textIndex = track.length-1;
+        int patternIndex = 0; //final pattern is flipped at input so iterate pattern positively
+        int patternToMatch = groundTricks.length-1; //last pattern
+        int matchStart = -1;
+        //iterate through text
+        while(textIndex > stop){
+            //if lettermatch increment both text and pattern index
+            if(track[textIndex] == groundTricks[patternToMatch][patternIndex]){
+                textIndex--;
+                patternIndex++;
+                //if reached end of final pattern then break
+                if(patternIndex == groundTricks[patternToMatch].length){
+                    matchStart = groundTricks[patternToMatch].length + textIndex + 2;
+                    break;
+                }
+            }
+            else{//mismatch
+                //if looking for first patternletter move to next textletter
+                if(patternIndex == 0){
+                    textIndex--;
+                }
+                else{//not looking for first letter follow links
+                    patternIndex = failLinks[patternToMatch][patternIndex - 1];
+                }
+            }
+        }
+
+        return matchStart;
+    }
+
+    private static int[] kmpAlgorithm1(int[][] groundTricks, int[][] failLinks, int[] track) {
+        //KMP algorithm:
+        int textIndex = 0;
+        int patternIndex = 0;
+        int patternToMatch = 0;
+        int matchStart = 0;
+        int matchLength = 0;
+        //iterate through text
+        while(textIndex < track.length){
+            //if lettermatch increment botch text and pattern index
+            if(track[textIndex] == groundTricks[patternToMatch][patternIndex]){
+                textIndex++;
+                patternIndex++;
+
+                //if reached end of pattern a ground trick is done and need airtime atleast 1 and start matching next pattern
+                if(patternIndex == groundTricks[patternToMatch].length){
+                    //if first pattern found then set matchStart to start of matched pattern
+                    if(patternToMatch == 0){
+                        matchStart = (textIndex - patternIndex) + 1 ;
+                    }
+                    //move to next pattern
+                    patternToMatch++;
+                    //set index to look for start letter (of next pattern)
+                    patternIndex = 0;
+                    //if all but last pattern found then break
+                    if(patternToMatch == groundTricks.length - 1){
+                        matchLength = (textIndex - matchStart) + 1;
+                        break;
+                        //patternToMatch = groundTricks.length - 1;
+                        //patternIndex = groundTricks[patternToMatch].length - 1;
+                    }
+                    else{
+                        //skip one textIndex as airtime
+                        textIndex++;
+                    }
+
+                }
+            }
+            else{//mismatch
+                //if looking for first patternletter move to next textletter
+                if(patternIndex == 0){
+                    textIndex++;
+                }
+                else{//not looking for first letter follow links
+                    patternIndex = failLinks[patternToMatch][patternIndex - 1];
+                }
+            }
+        }
+        if(matchLength == 0){
+            //System.out.println("Impossible");
+            return new int[]{-1,-1};
+        }
+        else{
+            //System.out.println(matchStart + " " + matchLength);
+            return new int[]{matchStart, matchLength};
+        }
+    }
+
+    private static void printArray(int[][] inputArray, String message) {
+        System.out.println(message);
+        for (int i = 0; i < inputArray.length; i++) {
+            for (int j = 0; j < inputArray[i].length; j++) {
+                System.out.print(inputArray[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int[][] generateFailLinks(int[][] groundTricks){
+        //generate faillinks
+        int[][] failLinks = new int[groundTricks.length][];
         //for each pattern
-        for (int x = 0; x < numGroundPhases; x++) {
+        for (int x = 0; x < groundTricks.length; x++) {
             //create j (left) and i (right) and link array for the pattern. link[0] always = 0;
             int j = 0, i = 1;
             failLinks[x] = new int[groundTricks[x].length];
@@ -87,70 +210,7 @@ public class KMP_Mandatory2 {
                 }
             }
         }
-
-        //print faillinks
-        /*System.out.println("faillinks:");
-        for (int i = 0; i < failLinks.length; i++) {
-            for (int j = 0; j < failLinks[i].length; j++) {
-                System.out.print(failLinks[i][j] + " ");
-            }
-            System.out.println("");
-        }*/
-
-        //KMP algorithm:
-        int textIndex = 0;
-        int patternIndex = 0;
-        int patternToMatch = 0;
-        int matchStart = 0;
-        int matchLength = 0;
-        //iterate through text
-        while(textIndex < track.length){
-            //if lettermatch increment botch text and pattern index
-            if(track[textIndex] == groundTricks[patternToMatch][patternIndex]){
-                textIndex++;
-                patternIndex++;
-
-                //if reached end of pattern a ground trick is done and need airtime atleast 1 and start matching next pattern
-                if(patternIndex == groundTricks[patternToMatch].length){
-                    //if first pattern found then set matchStart to start of matched pattern
-                    if(patternToMatch == 0){
-                        matchStart =(textIndex - patternIndex) + 1 ;
-                    }
-                    //move to next pattern
-                    patternToMatch++;
-                    //set index to look for start letter (of next pattern)
-                    patternIndex = 0;
-                    //if all patterns matched (success) then go back to look for another last pattern
-                    if(patternToMatch == groundTricks.length ){
-                        matchLength = (textIndex - matchStart) + 1;
-                        patternToMatch = groundTricks.length - 1;
-                        patternIndex = groundTricks[patternToMatch].length - 1;
-                    }
-                    else{
-                        //skip one textIndex as airtime
-                        textIndex++;
-                    }
-
-                }
-            }
-            else{//mismatch
-                //if looking for first patternletter move to next textletter
-                if(patternIndex == 0){
-                    textIndex++;
-                }
-                else{//not looking for first letter follow links
-                    patternIndex = failLinks[patternToMatch][patternIndex - 1];
-                }
-            }
-        }
-        if(matchLength == 0){
-            System.out.println("Impossible");
-        }
-        else{
-            System.out.println(matchStart + " " + matchLength);
-        }
-
-
+        return failLinks;
     }
 }
 /*
